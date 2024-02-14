ef22eeb28e8a4da6d08ef22b3d0193c39f515ec7
gleg0r <egor103230@gmail.com>
2024-02-14 20:10:18 +0300

    add: adding block in html from js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './scss/index.scss';
 import '../index.html';
 import Slider from './js/slider';
+import { genreData } from './db/genreData';
 import GenreList from './js/genreList';
 
 const sliderDots = document.querySelector('.slider-dots');
 const sliderBlock = document.querySelectorAll('.slider-block');
+const listBlock = document.querySelector('.books-list');
 
+const genreList = new GenreList(genreData, listBlock);
 const slider = new Slider(sliderDots, sliderBlock);
 
 slider.initDots();
 slider.sliderInterval();
+
+genreList.createList();
--- src/js/genreList.js
+++ src/js/genreList.js
@@ -1,7 +1,20 @@
 export default class GenreList {
-    constructor(data) {
-        this.data = data;
-    }
+  constructor(data, listBlock) {
+    this.data = data;
+    this.listBlock = listBlock;
+  }
 
-
+  createList() {
+    this.listBlock.innerHTML = 
+    `<ul>
+        ${this.data.map((item) => {
+    return (`<li>${item}</li>`);
+  })}
+    </ul>`;
+  }
 }
+
+
+// Министерство строительсвто и ЖКХ раздел ценообразования
+// Федеральной государственной ифнормационной системы ценообразования в строительстве (ФГИС ЦС)
+// Официальный сайт комитета города Москвы по ценовой политике и государственной экспертизе проектов
